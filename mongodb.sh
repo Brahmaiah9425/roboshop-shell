a147213d109d1d894f1d5cef652ee51d946cbd08
Brahmaiah <bramhithangedupalli@gmail.com>
2024-10-31 18:20:55 +0530

    Updated submodule roboshop-shell

--- mongodb.sh
+++ mongodb.sh
@@ -12,7 +12,8 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-    if [ "$1" -ne 0 ]; then
+    if [ $1 -ne 0 ]
+    then
         echo -e "$2 ... $R FAILED $N"
         exit 1
     else
@@ -20,13 +21,14 @@ VALIDATE(){
     fi
 }
 
-if [ "$ID" -ne 0 ]; then
+if [ $ID -ne 0 ]
+then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1 # you can give other than 0
 else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-cp /path/to/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo"
